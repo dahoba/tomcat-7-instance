6a53eabbcb295f1f88392d39b903c3ec76c0ee0a
Siritas Suvadhanabhakdi <dahoba@gmail.com>
2016-12-08 12:08:12 +0700

    * Remove un-necessary in sh script * Instruction how to config the folder in README

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# tomcat-7-instance
+# Tomcat 7 instance
 A tomcat 7 instance skeleton folder
+
+Execute these command to create the essential folder
+
+```
+mkdir -p lib logs temp webapps work/Catalina/localhost conf/Cataliba/localhost
+```
+
+Modify `JDK_HOME`, `JRE_HOME` in `starup.sh`, `shutdown.sh` to your JDK installation path.
--- shutdown.sh
+++ shutdown.sh
@@ -1,9 +1,3 @@
  ./tomcat-instance-common-config
 export CATALINA_BASE=.
 $CATALINA_HOME/bin/shutdown.sh
-#sleep 3 
-#if ps -ef|grep org.apache.catalina.startup.Bootstrap| grep  tomcat | grep -v grep; then
-#    echo "kill tomcat instances"
-#    echo `ps -ef|grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{print $2}' `
-#    ps -ef|grep org.apache.catalina.startup.Bootstrap | grep -v grep | awk '{print $2}' | xargs kill -9
-#fi
